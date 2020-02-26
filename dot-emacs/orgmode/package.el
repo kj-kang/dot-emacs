be9adced70bebf48865cd0a2feb6c5a7ab2732b9
jayden-c <jayden.c@kakaoenterprise.com>
2020-02-26 20:04:14 +0900

    org-mode 내 dot 기능 추가

--- dot-emacs/orgmode/package.el
+++ dot-emacs/orgmode/package.el
@@ -20,14 +20,21 @@
            "|"
            "DONE(d)"
            "CANCELLED(c@)")))
+
   (setq org-agenda-files
 	'("~/.org/inbox.org"
 	  "~/.org/home.org"
 	  "~/.org/works.org"
 	  "~/.org/study.org"
 	  "~/.org/kakao-data.org"))
+
   (setq org-refile-targets
-        '((org-agenda-files :maxlevel . 1))))
+        '((org-agenda-files :maxlevel . 1)))
+
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((dot . t)))
+  )
 
 (use-package helm-org
   :ensure t)
